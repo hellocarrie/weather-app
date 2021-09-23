2720be21433fa2a29ab829b298fd5d2be142b84b
Carrie <kari2742@gmail.com>
2021-09-23 16:19:52 -0700

    modified date to display month, day and year. Added javascript for the Search Function

--- src/app.js
+++ src/app.js
@@ -1,3 +1,54 @@
+function searchCity(event) {
+  let city = document.querySelector("#city-input").value;
+
+  let apiKey = "502951590779e9a44b221563a4491245";
+
+  let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  event.preventDefault();
+  axios.get(apiURL).then(displayTemperature);
+}
+
+function formatDate(timestamp) {
+  //format Friday 10:00
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let mins = date.getMinutes();
+  let day = date.getDay();
+  let days = [
+    "sunday",
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thursday",
+    "friday",
+    "saturday",
+  ];
+  let month = date.getMonth();
+  let months = [
+    "january",
+    "februfary",
+    "march",
+    "april",
+    "may",
+    "june",
+    "july",
+    "august",
+    "september",
+    "October",
+    "November",
+    "December",
+  ];
+  let calDay = date.getDate();
+  let year = date.getFullYear();
+
+  console.log(day);
+  console.log(date);
+  console.log(month);
+  console.log(calDay);
+  console.log(year);
+  return `<li>${days[day]} ${hours}:${mins}</li> <li>${months[month]} ${calDay} ${year}</li>`;
+}
+
 function displayTemperature(response) {
   console.log(response);
   console.log(response.data.weather[0].description);
@@ -7,15 +58,14 @@ function displayTemperature(response) {
   let cityElement = document.querySelector("#city");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date-time");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   descriptionElement.innerHTML = response.data.weather[0].description;
   cityElement.innerHTML = response.data.name;
   humidityElement.innerHTML = `${response.data.main.humidity}%`;
   windElement.innerHTML = `${response.data.wind.speed} km/h`;
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
-apiKey = "502951590779e9a44b221563a4491245";
-apiURL = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
-
-console.log(apiURL);
-axios.get(apiURL).then(displayTemperature);
+let form = document.querySelector("#city-search");
+form.addEventListener("submit", searchCity);
